5033f34c5d2266ee4b7ef34bbc0728390f0dba53
cjen1-msft <chrisjensen@microsoft.com>
2026-03-23 18:01:17 +0000

    provider->backend

--- build.rs
+++ build.rs
@@ -1,12 +1,12 @@
 fn main() {
     println!("cargo:rustc-check-cfg=cfg(sync_crypto)");
     println!("cargo:rustc-check-cfg=cfg(async_crypto)");
-    println!("cargo:rustc-check-cfg=cfg(crypto_provider, values(\"crypto_openssl\", \"crypto_pure_rust\"))");
+    println!("cargo:rustc-check-cfg=cfg(crypto_backend, values(\"crypto_openssl\", \"crypto_pure_rust\"))");
 
     let has_openssl = std::env::var_os("CARGO_FEATURE_CRYPTO_OPENSSL").is_some();
     let has_pure_rust = std::env::var_os("CARGO_FEATURE_CRYPTO_PURE_RUST").is_some();
 
-    let crypto_provider = if has_pure_rust {
+    let crypto_backend = if has_pure_rust {
         Some("crypto_pure_rust")
     } else if has_openssl {
         Some("crypto_openssl")
@@ -14,9 +14,9 @@ fn main() {
         None
     };
 
-    if let Some(crypto_provider) = crypto_provider {
+    if let Some(crypto_backend) = crypto_backend {
         println!("cargo:rustc-cfg=sync_crypto");
         println!("cargo:rustc-cfg=async_crypto");
-        println!("cargo:rustc-cfg=crypto_provider=\"{crypto_provider}\"");
+        println!("cargo:rustc-cfg=crypto_backend=\"{crypto_backend}\"");
     }
 }
