373b07c58c2169919271283ab87162bc66e7b327
cjen1-msft <chrisjensen@microsoft.com>
2026-06-16 11:59:12 +0100

    async in docstrings

--- aci/src/lib.rs
+++ aci/src/lib.rs
@@ -82,7 +82,7 @@ const MAX_GUEST_VMPL: u32 = 3;
 ///     uvm,
 ///     "ContainerPlat-AMD-UVM",
 ///     minimum_uvm_svn,
-/// ).await?;
+/// )?;
 /// # let _ = verified_report_data;
 /// # Ok(())
 /// # }
@@ -229,7 +229,7 @@ pub mod sync {
 ///     uvm,
 ///     "ContainerPlat-AMD-UVM",
 ///     minimum_uvm_svn,
-/// )?;
+/// ).await?;
 /// # let _ = verified_report_data;
 /// # Ok(())
 /// # }
